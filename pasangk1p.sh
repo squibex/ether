f8ab84103f39f17afb6219e5e8a4a82fbf30aedd
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 09:22:46 +0700

    Update pasangk1p.sh

--- pasangk1p.sh
+++ pasangk1p.sh
@@ -6,5 +6,5 @@ wget https://github.com/squibex/ether/raw/main/k1p.sh
 wget https://github.com/squibex/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x ether.sh
-./ether.sh
+chmod +x k1p.sh
+./k1p.sh
